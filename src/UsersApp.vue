ecf95f4730edbf826c80970fd788670c40562b1d
riku <em@il>
2017-03-18 20:36:22 +0900

    Fix navbar perfect!

---
+++ src/UsersApp.vue
@@ -0,0 +1,12 @@
+<template>
+    $TEMPLATE$
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import Component from 'vue-class-component'
+
+    @Component
+    export default class UsersApp extends Vue {
+    }
+</script>
